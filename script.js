c475fcc035398cf011ade05b8a06ece23a6d1deb
attoh alexander <alexander.attoh22@gmail.com>
2022-11-11 07:43:17 +0100

    update

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const dotsNav = document.querySelector('.carousel_nav');
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
-// console.log(slideWidth);
 
 // arrange slides next to each other
 const setSlidePostion = (slide, index) => {
@@ -22,23 +21,27 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
 
 const updateDots = (currentDot, targetDot) => {
     currentDot.classList.remove('current-slide');
-    targetDot.classList.add('current-slide')
+    targetDot.classList.add('current-slide');
 }
 
-const hideShowArrows = (track, slides, targetIndex) => {
-    if (targetIndex == 0) {
+const hideShowArrows = (track, slides, targetIndex, currentSlide, targetSlide, currentDot, targetDot) => {
+    console.log(targetDot)
+    if (targetIndex == 8) {
         const currentSlide = slides[0];
-        const targetSlide = slides[targetIndex];
-        moveToSlide(track, currentSlide, targetSlide, currentSlide, targetSlide)
-    } else if (targetIndex === slides.length -1) {
-        const currentSlide = slides[targetIndex];
+        const targetSlide = slides[slides.length-1];
+        const targetDot = dotsNav.querySelector('.last-slide');
+        moveToSlide(track, currentSlide, targetSlide);
+        updateDots(currentDot, targetDot);
+    } else if (targetIndex === -1) {
+        const currentSlide = slides[slides.length-1];
         const targetSlide = slides[0];
-        moveToSlide(track, currentSlide, targetSlide)
+        const targetDot = dotsNav.querySelector('.first-slide');
+        moveToSlide(track, currentSlide, targetSlide);
+        updateDots(currentDot, targetDot);
+    } else {
+        moveToSlide(track, currentSlide, targetSlide);
+        updateDots(currentDot, targetDot);
     }
-
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-    currentSlide.classList.remove('current-slide');
-    targetSlide.classList.add('current-slide');
 }
 
 
@@ -51,8 +54,8 @@ prevbtn.addEventListener('click', e => {
     const prevIndex = slides.findIndex(slide => slide === prevSlide);
 
     // moveToSlide(track, currentSlide, prevSlide);
-    updateDots(currentDot, prevDot);
-    hideShowArrows(track, slides, prevIndex, currentSlide, prevSlide);
+    hideShowArrows(track, slides, prevIndex, currentSlide, prevSlide, currentDot, prevDot);
+    // updateDots(currentDot, prevDot);
 })
 
 // when i click right move slide to the right
@@ -64,8 +67,8 @@ nextbtn.addEventListener('click', e => {
     const nextIndex = slides.findIndex(slide => slide === nextSlide);
 
     // moveToSlide(track, currentSlide, nextSlide);
-    updateDots(currentDot, nextDot);
-    hideShowArrows(track, slides, nextIndex, currentSlide, nextSlide);
+    hideShowArrows(track, slides, nextIndex, currentSlide, nextSlide, currentDot, nextDot);
+    // updateDots(currentDot, nextDot);
 })
 
 
@@ -83,7 +86,7 @@ dotsNav.addEventListener('click', e => {
     const targetSlide = slides[targetIndex];
     
     // moveToSlide(track, currentSlide, targetSlide);
-    updateDots(currentDot, targetDot);
-    hideShowArrows(track, slides, targetIndex, currentSlide, targetSlide);
+    hideShowArrows(track, slides, targetIndex, currentSlide, targetSlide, currentDot, targetDot);
+    // updateDots(currentDot, targetDot);
     
 })
